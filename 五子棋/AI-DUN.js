ba7c9ab3a2d0bdaa3bd7112744bb3aa6465e97ce
LD <475098936@qq.com>
2016-05-21 12:24:31 +0800

    写的一些游戏demo
    2333

---
+++ 五子棋/AI-DUN.js
@@ -0,0 +1,15 @@
+/**
+ * Created by 123 on 2015/6/11.
+ */
+var AI={
+    init:function(){
+        AI.whatnext=0;
+        AI.next=0
+    },
+    think:function(){
+
+    },
+    fall:function(){
+        AI.next
+    }
+};
